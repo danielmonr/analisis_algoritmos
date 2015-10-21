ef831ee9f3e4fa7e73c972cff5d86744329c6da6
daniel <danielmonr@gmail.com>
2015-10-21 12:29:22 -0500

    Problema Practico 2 -casi terminado boost-

--- Problema2/boostGraph.cpp
+++ Problema2/boostGraph.cpp
@@ -17,6 +17,8 @@
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <boost/graph/graph_archetypes.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/config.hpp>
 #include <boost/graph/directed_graph.hpp>
@@ -30,6 +32,10 @@
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/johnson_all_pairs_shortest.hpp>
 #include <algorithm>
+#include <utility>
+#include <array>
+#include <fstream>
+#include <iomanip>
 #include <time.h>
 
 
@@ -93,9 +99,45 @@ int main(){
 		cout << "(" << index[source(*ei,g)] << "," << index[target(*ei,g)] << ")" << endl;
 	}
 
+
 	cout << "Busqueda de profundidad" << endl;
 	default_dfs_visitor v;
+	t = clock();
 	depth_first_search(g, visitor(v));
+	t = clock() -t;
+	cout << "Tiempo: " << (((float)t)/ CLOCKS_PER_SEC) << " segundos." << endl;
+
+	cout << "Busqueda de amplitud" << endl;
+	Graph::vertex_descriptor nodo_i = vertex(1,g);
+	default_bfs_visitor v2;
+	t = clock();
+	breadth_first_search(g,nodo_i,visitor(v2));
+	t = clock() - t;
+	cout << "Tiempo: " << (((float)t)/ CLOCKS_PER_SEC) << " segundos." << endl;
+
+	cout << "Primm" << endl;
+	vector<nodo_t> p(num_vertices(g));
+	t = clock();
+	prim_minimum_spanning_tree(g, &p[0]);
+	t = clock() - t;
+	cout << "Tiempo: " << (((float)t)/ CLOCKS_PER_SEC) << " segundos." << endl;
+
+	cout << "Kruskal" << endl;
+	vector<arista_t> sol;
+	t = clock();
+	kruskal_minimum_spanning_tree(g, back_inserter(sol));
+	t = clock() -t;
+
+	cout << "Dijkstra" << endl;
+	vector<nodo_t> p2(num_vertices(g));
+	vector<int> d(num_vertices(g));
+	nodo_t s = vertex(nodo_i, g);
+	t = clock();
+	dijkstra_shortest_paths(g,s, predecessor_map(make_iterator_property_map(p2.begin(), get(vertex_index, g))).distance_map(make_iterator_property_map(d.begin(), get(vertex_index, g))));
+	t = clock() -t;
+	cout << "Tiempo: " << (((float)t)/ CLOCKS_PER_SEC) << " segundos." << endl;
+
+	cout << "Floyd-Warshall" << endl;
 
 	return 0;
 }
